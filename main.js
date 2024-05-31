1ae5425e446d6b34a0e90118cfba9620d40e6beb
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-05-31 01:45:59 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+let userName = "sadia kanwal";
+let password = 12345;
+condition: userName == "sadia kanwal" && password == 1234
+    ? console.log("your account is logged in")
+    : console.log("invalid username and password");
+let atmPinCode = 1357;
+condition: atmPinCode == 2588
+    ? console.log("transaction successfully completed")
+    : console.log("invalid pin code try again");
+let stundentsAchieveGrade = "A1";
+let percentageRequirement = 90;
+condition: stundentsAchieveGrade == "A1" || percentageRequirement == 88
+    ? console.log("you are eligible for scholarship")
+    : console.log("you are not eligible for scholarship");
---
+++ main.ts
@@ -0,0 +1,17 @@
+let userName:string = "sadia kanwal"; 
+let password:number = 12345;
+condition:userName == "sadia kanwal" && password == 1234
+?console.log("your account is logged in")
+:console.log("invalid username and password");
+
+let atmPinCode:number = 1357
+condition:atmPinCode == 2588
+?console.log("transaction successfully completed")
+:console.log("invalid pin code try again");
+
+let stundentsAchieveGrade= "A1";
+let percentageRequirement= 90;
+condition:stundentsAchieveGrade == "A1" || percentageRequirement == 88
+?console.log("you are eligible for scholarship")
+:console.log("you are not eligible for scholarship");
+
